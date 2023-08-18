bd68132af6b1c548bb13d692b017f5460b34d8f0
usman-ahmed99 <usman.ahmed99@hotmail.com>
2023-08-18 14:55:09 +0500

    test

---
+++ install_xcode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install the desired Xcode version
+xcversion install 14.3.1
+
+# Set the selected Xcode version
+sudo xcode-select -s '/Applications/Xcode_14.3.1.app/Contents/Developer'
